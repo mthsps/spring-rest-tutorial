6e8792c2848d34b3a202eb4da3cc34b754689fe8
Matheus <dmatheusps@gmail.com>
2022-04-27 23:01:50 -0300

    Evolving REST APIs

--- src/main/java/com/restdemo/payroll/Employee.java
+++ src/main/java/com/restdemo/payroll/Employee.java
@@ -10,60 +10,81 @@ import javax.persistence.Id;
 class Employee {
 
   private @Id @GeneratedValue Long id;
+  private String firstName;
+  private String lastName;
   private String name;
   private String role;
 
   Employee() {}
 
-  Employee(String name, String role) {
-
-    this.name = name;
+  Employee(String firstName, String lastName, String role) {
+	this.firstName = firstName;
+    this.lastName = lastName;
     this.role = role;
-  }
-
-  public Long getId() {
-    return this.id;
   }
 
   public String getName() {
-    return this.name;
-  }
+	    return this.firstName + " " + this.lastName;
+	  }
 
-  public String getRole() {
-    return this.role;
-  }
+	  public void setName(String name) {
+	    String[] parts = name.split(" ");
+	    this.firstName = parts[0];
+	    this.lastName = parts[1];
+	  }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	  public Long getId() {
+	    return this.id;
+	  }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	  public String getFirstName() {
+	    return this.firstName;
+	  }
 
-  public void setRole(String role) {
-    this.role = role;
-  }
+	  public String getLastName() {
+	    return this.lastName;
+	  }
 
-  @Override
-  public boolean equals(Object o) {
+	  public String getRole() {
+	    return this.role;
+	  }
 
-    if (this == o)
-      return true;
-    if (!(o instanceof Employee))
-      return false;
-    Employee employee = (Employee) o;
-    return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-        && Objects.equals(this.role, employee.role);
-  }
+	  public void setId(Long id) {
+	    this.id = id;
+	  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(this.id, this.name, this.role);
-  }
+	  public void setFirstName(String firstName) {
+	    this.firstName = firstName;
+	  }
 
-  @Override
-  public String toString() {
-    return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
-  }
+	  public void setLastName(String lastName) {
+	    this.lastName = lastName;
+	  }
+
+	  public void setRole(String role) {
+	    this.role = role;
+	  }
+
+	  @Override
+	  public boolean equals(Object o) {
+
+	    if (this == o)
+	      return true;
+	    if (!(o instanceof Employee))
+	      return false;
+	    Employee employee = (Employee) o;
+	    return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+	        && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
+	  }
+
+	  @Override
+	  public int hashCode() {
+	    return Objects.hash(this.id, this.firstName, this.lastName, this.role);
+	  }
+
+	  @Override
+	  public String toString() {
+	    return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+	        + '\'' + ", role='" + this.role + '\'' + '}';
+	  }
 }
